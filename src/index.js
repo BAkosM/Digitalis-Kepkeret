c248107db1f204d9a2e1a153e40a89dc9924b85a
Benedek Ákos Marcell <benedek.akos.marcell@gmail.com>
2022-09-28 13:43:37 +0200

    Dark mode add

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ function keretez() {
     //Test
     //console.log('katt');
     
-    let url = document.getElementById("KUrl").value;
+    let Kurl = document.getElementById("KUrl").value;
     let meret = parseFloat(document.getElementById("KWith").value);
     let keret = parseFloat(document.getElementById("KBorder").value);
     let szin = document.getElementById("KColor").value;
@@ -14,7 +14,20 @@ function keretez() {
     document.getElementById("kep").style.width =szel;
     document.getElementById("kep").style.border = border;
 }
+var onoff = 0;
+function darkmode() {
+    //Test
+    //console.log('katt');
+    if (onoff == 0) {
+        onoff = 1;
+        document.getElementById("test").classList.add("dark-mode");
+    }else {
+        onoff = 0;
+        document.getElementById("test").classList.remove("dark-mode");
+    }
+}
 function init(){
     document.getElementById("gomb").addEventListener('click', keretez);
+    document.getElementById("dark").addEventListener('click', darkmode);
 }
 document.addEventListener("DOMContentLoaded",init);
