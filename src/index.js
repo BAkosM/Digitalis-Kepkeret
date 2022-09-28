1e21f783a95d50dd3479a3b283f7ab06009df1d1
Benedek Ákos Marcell <benedek.akos.marcell@gmail.com>
2022-09-28 12:50:20 +0200

    button

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import './style.css';
 
+function keretez() {
+    let url = document.getElementById("KUrl").value;
+    let meret = parseFloat(document.getElementById("KWith").value);
+    let keret = parseFloat(document.getElementById("KBorder").value);
+    let szin = document.getElementById("KColor").value;
+    let border = keret+"px solid "+szin;
+
+    document.getElementById("kep").style.width = meret;
+    document.getElementById("kep").style.border = border;
+}
+function init(){
+    document.getElementById("gomb").addEventListener('click', keretez);
+}
+document.addEventListener("DOMContentLoaded",init);
