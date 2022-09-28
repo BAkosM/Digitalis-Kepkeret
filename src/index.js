ce11b4c08f4157879d8ca058e825a18bb0b0a553
Benedek Ákos Marcell <benedek.akos.marcell@gmail.com>
2022-09-28 13:09:23 +0200

    Html button fix

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import './style.css';
 
 function keretez() {
+    //Test
+    //console.log('katt');
+    
     let url = document.getElementById("KUrl").value;
     let meret = parseFloat(document.getElementById("KWith").value);
     let keret = parseFloat(document.getElementById("KBorder").value);
     let szin = document.getElementById("KColor").value;
+    let szel = meret+"px"
     let border = keret+"px solid "+szin;
 
-    document.getElementById("kep").style.width = meret;
+    document.getElementById("kep").style.width =szel;
     document.getElementById("kep").style.border = border;
 }
 function init(){
